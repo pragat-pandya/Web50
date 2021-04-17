edcf36f8f20366f0bb83e385be17c76060bcb9d6
Pragat Pandya <pragat.pandya@gmail.com>
2021-04-17 17:39:27 +0530

    Illustrate defining more than one view in our webapp

--- Django/hello/app/urls.py
+++ Django/hello/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path ("", views.index, name="index")
+  path ("", views.index, name="index"),
+  path ('pragat', views.pragat, name="pragat"),
+  path ('brian', views.brian, name="brian"),
+  path ('david', views.david, name = "david")
 ]
--- Django/hello/app/views.py
+++ Django/hello/app/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index (request):
   return HttpResponse("Hello, World!")
+
+def pragat (request):
+  return HttpResponse("Hello, Pragat!")
+
+def brian (request):
+  return HttpResponse("Hello, Brian!")
+
+def david (request):
+  return HttpResponse("Hello, David!")
