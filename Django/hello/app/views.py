496599391c3071527a1cb179a8fc57b33b3e3977
Pragat Pandya <pragat.pandya@gmail.com>
2021-04-17 20:33:24 +0530

    Using HTML text as HttpResponse

--- Django/hello/app/views.py
+++ Django/hello/app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index (request):
-  return HttpResponse("Hello, World!")
+  return HttpResponse("<h1 style=\"color: blue;\">Hello, World!</h1>")
 
 def pragat (request):
   return HttpResponse("Hello, Pragat!")
