de485ba594c4a718573644219b40b92721f15866
Pragat Pandya <pragat.pandya@gmail.com>
2021-04-17 17:48:58 +0530

    Add generalized greeting function intead of separate views for each person

--- Django/hello/app/urls.py
+++ Django/hello/app/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
   path ("", views.index, name="index"),
-  path ('pragat', views.pragat, name="pragat"),
-  path ('brian', views.brian, name="brian"),
-  path ('david', views.david, name = "david")
+  path ("<str:name>", views.greet, name="greet")
 ]
