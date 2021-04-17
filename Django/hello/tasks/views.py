7ac8fb5af5fe3f72a891d698acc56788b8cc4a20
Pragat Pandya <pragat.pandya@gmail.com>
2021-04-18 01:20:14 +0530

    Add form for adding new tasks to the task list using django forms

--- Django/hello/tasks/views.py
+++ Django/hello/tasks/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django import forms
 
 tasks = ["foo", "bar", "baz"]
+class NewTaskForm (forms.Form):
+  task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index (request):
   return render (request, "tasks/index.html", {
     "tasks" : tasks
   })
+
+def add (request):
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data["task"]
+      tasks.append(task)
+      return HttpResponseRedirect(reverse('tasks:index'))
+
+  else:
+    return render(request, "tasks/add.html", {
+      "form" : NewTaskForm()
+    })
