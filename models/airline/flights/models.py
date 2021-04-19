3a7c70b9d60061d9930f8f5585756a243bb50f5a
Pragat Pandya <pragat.pandya@gmail.com>
2021-04-20 00:53:18 +0530

    Add str represenation function of Flight class in flights.models

--- models/airline/flights/models.py
+++ models/airline/flights/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Flight (models.Model):
   origin = models.CharField(max_length=64)
   destination = models.CharField(max_length=64)
-  duraion = models.IntegerField()
+  duration = models.IntegerField()
+  
+  def __str__(self):
+    return f"{self.id}: {self.origin} to {self.destination}"
